687d9be6b96853e4c950b442f255226098e6257f
OporaRuSr <OporaRuSr@amberskynet.org>
2020-09-21 13:49:03 +0300

    fix viewport

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -11,6 +11,8 @@ export default function Layout(props) {
     <main>
       <Head>
         <title>{titleText}</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name='description' content={description}/>
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"/>
       </Head>
