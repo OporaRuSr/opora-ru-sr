93649f66d52afa7c47385f3b6815c5f1afff2fd6
OporaRuSr <OporaRuSr@amberskynet.org>
2020-09-21 13:55:41 +0300

    fix charSet

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -11,7 +11,7 @@ export default function Layout(props) {
     <main>
       <Head>
         <title>{titleText}</title>
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name='description' content={description}/>
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"/>
