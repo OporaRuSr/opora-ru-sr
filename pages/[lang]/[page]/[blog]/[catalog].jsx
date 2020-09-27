d2389d10306b5a4a9a1fc486ddcf29f758af82fa
nobody <no@mail.me>
2020-09-27 16:03:57 +0300

    some updates

--- pages/[lang]/[page]/[blog]/[catalog].jsx
+++ pages/[lang]/[page]/[blog]/[catalog].jsx
@@ -1,16 +1,26 @@
-import { getAllCatalogPageLinks } from '../../../../api'
+import {PAGE_TYPE} from '../../../../api/constants'
+import { getAllCatalogPageLinks, getBlogPage } from '../../../../api'
 
-export default function page(props) {
+import CatalogPage from '../../../../components/CatalogPage'
+import CatalogTags from '../../../../components/CatalogTags'
+
+export default function page({pageType, lang, payload}) {
+    const props = {lang, payload}
     console.log('[catalog].jsx props ', props)
-    return (
-        <h1>Hello</h1>
-    )
+    switch (pageType) {
+        case 'page': return (
+          <CatalogPage {...props}/>
+        )
+        case 'tags': return (
+          <CatalogTags {...props}/>
+        )
+    }
 }
 
 export async function getStaticPaths() {
-    console.log('[catalog]/index.jsx getStaticPaths')
+    // console.log('[catalog].jsx getStaticPaths')
     const catalogList = await getAllCatalogPageLinks()
-    console.log(catalogList)
+    // console.log(catalogList)
     return {
       paths: catalogList,
       fallback: false
@@ -18,8 +28,25 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-    console.log(params)
+    console.log('[catalog].jsx params:', params)
+    const { lang, page, blog:pageType, catalog:catalogPage } = params
+    console.log(lang, page, pageType, catalogPage)
+    if (pageType === 'tags') {
+        return {
+            props: {
+
+            }
+        }
+    }
+    
+    const slug = `${page}/${catalogPage}`
+    const payload = await getBlogPage({lang, slug})
+    console.log(payload)
     return {
-        props: {}
-      }
+        props: {
+            pageType,
+            lang,
+            payload
+        }
+    }
 }
